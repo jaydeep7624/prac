516370d639eff69a3bdd8007d059b7a1b21aa9c0
jaydeep <jaydeep7624@gmail.com>
2025-04-16 15:47:20 +0530

    new array method

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function sumOfAllNaturalNumber(n){
     }
     return sum;
 }
-// console.log(sumOfAllNaturalNumber(10));
+console.log(sumOfAllNaturalNumber(10));
 
 function sumOfDigitOfNumber(number){
     let sum=0;
@@ -42,7 +42,7 @@ function sumOfDigitOfNumber(number){
     }
     return sum;
 }
-// console.log(sumOfDigitOfNumber(1234));
+console.log(sumOfDigitOfNumber(1234));
 
 
 
@@ -86,8 +86,8 @@ function PairExist(numbers,sum){
     return flag1;
 }
 // console.log("saa",secondLargestNumber([10,20,30,30]));
-// console.log("saa",secondLargestNumber([1,1,1,1,1]));
-// console.log("saa",PairExist([1,2,3],2));
+console.log("saa",secondLargestNumber([1,1,1,1,1]));
+console.log("saa",PairExist([1,2,3],2));
  
 
 function bubbleSort(array){
@@ -102,7 +102,7 @@ function bubbleSort(array){
     }    
     return array;
 }
-// console.log("Bubble sort ",bubbleSort([5,9,1,3,9,10]));
+console.log("Bubble sort ",bubbleSort([5,9,1,3,9,10]));
 
 function selectionSort(array){
     for(let i=0;i<array.length-1;i++){
@@ -119,7 +119,7 @@ function selectionSort(array){
     }
     return array;
 }
-// console.log("selection sort :",selectionSort([10,20,12,2,21]));
+console.log("selection sort :",selectionSort([10,20,12,2,21]));
 
 
 function quickSort(array){
@@ -141,7 +141,7 @@ function quickSort(array){
     console.log(left,pivot,right);
     return [...quickSort(left),pivot,...quickSort(right)];
 }
-// console.log("Quick Sort :",quickSort( [7, 2, 1, 6, 8, 5, 3, 4]));
+console.log("Quick Sort :",quickSort( [7, 2, 1, 6, 8, 5, 3, 4]));
 
 function exactdiscountAmount(array,target){
     let item=[];
@@ -166,3 +166,365 @@ function exactdiscountAmount(array,target){
     return uniqueArr;
 }
 console.log("discount amount array is",exactdiscountAmount([1,2,1,3,2],4));
+// console.log("discount amount array is",exactdiscountAmount([1,2,1,3,2],4));
+
+const data = [ [1, 'apple',200], [2, 'banana',300], [1, 'orange',400], [2, 'grape',100], [3, 'melon',200] ];
+/*
+{
+    1: ['apple', 'orange'],
+    2: ['banana', 'grape'],
+    3: ['melon']
+}*/
+
+let item = {};
+data.forEach(([key, value,price]) => {
+  if (!item[key]) {
+    item[key] = [];
+  }
+  item[key].push(value);
+});
+console.log(item)
+
+
+const colors = ['red', 'blue', 'red', 'green', 'blue', 'blue'];
+/*
+{
+    red: 2,
+    blue: 3,
+    green: 1
+}*/
+
+let color_with_count={};
+colors.forEach((item)=>{
+    if(!color_with_count[item]){
+        color_with_count[item]=0;
+    }
+    color_with_count[item]++;
+})
+console.log(color_with_count);
+
+const nums = [1, 3, 2, 3, 4, 3, 2, 1, 1, 1];
+/*
+{
+    number: 1,
+    count: 4
+}
+*/
+
+let number_with_count= {}
+nums.forEach((item)=>{
+    if(!number_with_count[item]){
+        number_with_count[item]=0;
+    }
+    number_with_count[item]++;
+})
+console.log(number_with_count);
+
+let maxCount=0;
+let mostFrequest=null;
+for(item in number_with_count){
+   if(number_with_count[item]>maxCount){
+       maxCount = number_with_count[item];
+       mostFrequest =Number(item);
+   }
+}
+console.log(mostFrequest,maxCount);
+
+const messyArray = [0, "apple", false, "", 42, null, "banana", undefined, NaN];
+// ["apple", 42, "banana"]
+let array=[]
+messyArray.forEach(item=>{
+    if(item){
+        array.push(item);
+    } 
+})
+console.log(array) ;   
+
+const nested = [1, [2, [3, [4, [[5]],8]]]];
+function flattenArray(array){
+    let result =[];
+    array.forEach(item=>{
+        console.log("Item",item);
+        if(Array.isArray(item)){
+            result = result.concat(flattenArray(item));
+            console.log("Result ",result);   
+        }
+        else{
+            console.log("Push thai item",item);
+            result.push(item);
+        }
+    })
+    return result;
+}
+
+
+console.log("Flat array",nested.flat(Infinity));
+console.log("Flat array",flattenArray(nested));
+
+const words = ['Apple', 'Banana', 'apple', 'ORANGE', 'banana', 'Orange'];
+// ['Apple', 'Banana', 'ORANGE']
+
+let word_count={}
+let word_array=[];
+words.forEach(item=>{
+    let loew_item= item.toLowerCase();
+    if(!word_count[loew_item]){
+        word_count[loew_item]=true;
+        word_array.push(loew_item)
+    }
+})
+console.log(word_array);
+
+const anagram_words = ['listen', 'silent', 'enlist', 'rat', 'tar', 'art', 'hello'];
+/*[
+    ['listen', 'silent', 'enlist'],
+    ['rat', 'tar', 'art'],
+    ['hello']
+]*/
+
+let anagram_item={};
+
+anagram_words.forEach(item=>{
+    let new_item =item.split('').sort().join('');
+    if(!anagram_item[new_item]){
+        anagram_item[new_item]=[];
+    }
+    anagram_item[new_item].push(new_item);
+})
+console.log(Object.values(anagram_item))
+
+
+// Find two stirng is Anagram Or not : 
+function string_anagram(str1,str2){
+    let anagram_str1=str1.split('').sort().join('');
+    let anagram_str2=str2.split('').sort().join('');
+    if(anagram_str1 === anagram_str2){
+        return true;
+    }
+    return false;
+}
+console.log(string_anagram('hello','oellh'));
+
+const str = "aabbcdeff";
+//  "c"
+
+function uniqueCharacter(str){
+    let unique_character_item={};
+    let str_array = str.split('');
+    str_array.forEach(item=>{
+        if(!unique_character_item[item]){
+            unique_character_item[item]=0;
+        }
+        unique_character_item[item]=unique_character_item[item]+1;
+    })
+    for(let item in unique_character_item){
+        if(unique_character_item[item]==1){
+            return item;
+        }
+    }   
+}
+console.log(uniqueCharacter(str));
+
+/*
+Input: nums = [2, 7, 11, 15], target = 9
+Output: [0, 1]
+*/
+
+let number=[2,7,11,15];
+let target=9
+let array1=[];
+for(let i=0;i<number.length-1;i++){
+    
+    for(let j=i+1;j<number.length;j++){
+        if((number[i]+number[j])==target){
+            array1.push(i);
+            array1.push(j);
+            break;
+        }
+    }
+}
+console.log(array1)
+
+/*
+Input: nums = [0, 1, 0, 3, 12]
+Output: [1, 3, 12, 0, 0]
+*/
+
+function moveZeros(array){
+    let length = array.length;
+    for(let i=0;i<length;i++){
+     
+        console.log("Elements",array[i])
+        if(array[i]==0){
+            array.splice(i,1);
+            array.push(0);
+            i--; 
+            length--;
+        }   
+    }
+    console.log(array);
+}
+moveZeros([0,1,0,0,3,12]);
+
+function rotateArray(array,k){
+    if(k>0){
+        for(let i=0;i<k;i++){
+           let item = array.pop();
+           array.unshift(item);
+           console.log("array",array);
+        }
+    }
+    return array    
+}
+console.log(rotateArray([1,2,3,4,5],4));
+
+// Count No of occurences .
+function countOccurences(array){
+    let count_item={};
+    array.forEach(item=>{
+        if(!count_item[item]){
+            count_item[item]=0;
+        }
+        count_item[item]++;
+    })
+    return count_item;
+}
+console.log(countOccurences([1,2,3,4,5,1,2,3,4,5]));
+// Find the longest substring without repeating characters
+function longestSubstring(str){
+    let longest_substring = '';
+    let current_substring = '';
+    for(let i=0;i<str.length;i++){
+        if(current_substring.includes(str[i])){
+            current_substring = current_substring.slice(current_substring.indexOf(str[i])+1);
+        }
+        current_substring += str[i];
+        if(current_substring.length>longest_substring.length){
+            longest_substring = current_substring;
+        }
+    }
+    return longest_substring;
+}
+console.log(longestSubstring('abcabcbb')); // Output: 'abc'
+// Find the first non-repeating character in a string
+function firstNonRepeating(str){
+    let char_count = {};
+    for(let i=0;i<str.length;i++){
+        if(!char_count[str[i]]){
+            char_count[str[i]]=0;
+        }
+        char_count[str[i]]++;
+    }
+    for(let i=0;i<str.length;i++){
+        if(char_count[str[i]]==1){
+            return str[i];
+        }
+    }
+    return null;
+}
+console.log(firstNonRepeating('swiss')); // Output: 'w'
+// Find the longest palindrome in a string
+function longestPalindrome(str){
+    let longest_palindrome = '';
+    for(let i=0;i<str.length;i++){
+        for(let j=i;j<str.length;j++){
+            let substring = str.slice(i,j+1);
+            if(substring === substring.split('').reverse().join('') && substring.length>longest_palindrome.length){
+                longest_palindrome = substring;
+            }
+        }
+    }
+    return longest_palindrome;
+}
+console.log(longestPalindrome('babad')); // Output: 'bab' or 'aba'
+// Find the maximum product of two integers in an array
+function maxProduct(array){
+    let max_product = -Infinity;
+    for(let i=0;i<array.length;i++){
+        for(let j=i+1;j<array.length;j++){
+            let product = array[i]*array[j];
+            if(product>max_product){
+                max_product = product;
+            }
+        }
+    }
+    return max_product;
+}
+console.log(maxProduct([1,2,3,4,5])); // Output: 20
+// Find the intersection of two arrays
+function intersection(array1,array2){
+    let intersection_array = [];
+    for(let i=0;i<array1.length;i++){
+        if(array2.includes(array1[i]) && !intersection_array.includes(array1[i])){
+            intersection_array.push(array1[i]);
+        }
+    }
+    return intersection_array;
+}
+console.log(intersection([1,2,3,4],[3,4,5,6])); // Output: [3, 4]
+// Find the union of two arrays
+function union(array1,array2){
+    let union_array = [...array1];
+    for(let i=0;i<array2.length;i++){
+        if(!union_array.includes(array2[i])){
+            union_array.push(array2[i]);
+        }
+    }
+    return union_array;
+}
+console.log(union([1,2,3,4],[3,4,5,6])); // Output: [1, 2, 3, 4, 5, 6]
+// Find the difference of two arrays
+function difference(array1,array2){
+    let difference_array = [];
+    for(let i=0;i<array1.length;i++){
+        if(!array2.includes(array1[i])){
+            difference_array.push(array1[i]);
+        }
+    }
+    return difference_array;
+}
+console.log(difference([1,2,3,4],[3,4,5,6])); // Output: [1, 2]
+// Find the symmetric difference of two arrays
+function symmetricDifference(array1,array2){
+    let symmetric_difference_array = [];
+    for(let i=0;i<array1.length;i++){
+        if(!array2.includes(array1[i])){
+            symmetric_difference_array.push(array1[i]);
+        }
+    }
+    for(let i=0;i<array2.length;i++){
+        if(!array1.includes(array2[i])){
+            symmetric_difference_array.push(array2[i]);
+        }
+    }
+    return symmetric_difference_array;
+}
+console.log(symmetricDifference([1,2,3,4],[3,4,5,6])); // Output: [1, 2, 5, 6]
+// Find the longest common prefix of an array of strings
+function longestCommonPrefix(array){
+    if(array.length==0){
+        return '';
+    }
+    let prefix = array[0];
+    for(let i=1;i<array.length;i++){
+        while(array[i].indexOf(prefix)!==0){
+            prefix = prefix.slice(0,-1);
+        }
+    }
+    return prefix;
+}
+console.log(longestCommonPrefix(['flower','flow','flight'])); // Output: 'fl'
+// Find the longest common suffix of an array of strings
+function longestCommonSuffix(array){
+    if(array.length==0){
+        return '';
+    }
+    let suffix = array[0];
+    for(let i=1;i<array.length;i++){
+        while(array[i].lastIndexOf(suffix)!==array[i].length-suffix.length){
+            suffix = suffix.slice(1);
+        }
+    }
+    return suffix;
+}
+console.log(longestCommonSuffix(['flower','tower','power'])); // Output: 'ower'
